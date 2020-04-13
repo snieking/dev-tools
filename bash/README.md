21cfe12ccab8a621e459132d7f9675c28695027b
Viktor Plane <viktorplane.sonie@gmail.com>
2020-04-13 11:07:48 +0200

    Add bash alias

---
+++ bash/README.md
@@ -0,0 +1,7 @@
+# Bash
+
+## Source aliases
+
+```
+for f in /Users/viktorplane/dev-tools/bash/*-alias.sh; do source $f; done
+```
---
+++ bash/git-alias.sh
@@ -0,0 +1,1 @@
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
